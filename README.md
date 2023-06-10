ed44b9b210c220790a0c53acf2ac0650c98989e1
ktg5 <42522603+ktg5@users.noreply.github.com>
2023-06-10 11:34:09 -0700

    i think this is ready (v2 update)
    a lot of stuff, read up on the README.md to see how to setup the newer version...

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # [adult swim] bump bot
 A Twitter bot that posts random Adult Swim bumps.
 
+## V2 is out!!
+Since `twit.js` ain't workin' on my end, I gotta rewrite the bot usin' the `twitter-api-v2` npm module!!
+
+Just download the latest version whenever you can `:)`
+
+I've also made the bot do Twitter authentication in another file, `botAuth.js`, which is now how I authorize clients for my bots...
+
 ## How to work.
 1. Clone the repo - `git clone https://github.com/ktg5/asbumpbot`
 2. Install required packages - `npm i`
-3. Open `config.json` and replace all the values with your own. (`botUsername` and all four keys listed)
+3. Open `auth.json` and replace all the listed keys (api & access) with your own. 
+* ⚠⚠ Remember to **make sure your access keys are "`Created with Read, and Write permissions`" by enabling "`User authentication`" in your Twitter app's page!**
 4. Start the bot - `npm start`
--- bot.js
+++ bot.js
@@ -1,141 +1,85 @@
-const Twit = require('twit')
-var config = require('./config.json')
+const {config, client, logger} = require("./botAuth");
 const http = require('http');
 const fs = require('fs');
-var logger = require('winston');
-
-
-// Verify bot. 
-var bot = new Twit({
-  consumer_key:         config.consumer_key,
-  consumer_secret:      config.consumer_key_secret,
-  access_token:         config.access_token,
-  access_token_secret:  config.access_token_secret,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
-
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
 
 // getRandomArbitrary
 function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Make video file.
-var bumpNum = getRandomArbitrary(1, 8749);
-var url = `http://static.bumpworthy.com/bumps/${bumpNum}.d.mp4` // Get bump video file.
-var filepath = `media/${bumpNum}.mp4` // Set export locaiton.
-
-// Check to see if the video already exists.
-var checkFile = fs.existsSync(filepath)
-console.log(checkFile)
-if (checkFile = false) {
-  logger.error(`Media failed, a video file with the name is in the "media" folder. Restart to try again.`)
-  return;
+// Get bot Username (can only be called when Twitter wakes up)
+async function getUsername() {
+    return (await client.v2.me()).data.username;
 }
-
-const file = fs.createWriteStream(filepath);
-const request = http.get(url, (response) => {
-  response.pipe(file);
-
-  file.on('finish',() => { // When file is done downloaded.
-
-    file.close();
-    console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-    logger.info('Download Completed');
-
-    var filePath = `./${filepath}`
-    bot.postMediaChunked({ file_path: filePath }, function (err, data, response) {
-      console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-      if (err) {
-        logger.error("postMediaChunked error")
-        console.log(err)
-        return;
-      }
-
-      // Log media chunk info. (FOR DEBUG)
-      // logger.info("postMediaChunked data:")
-      // console.log(data)
-
-      const mediaIdStr = data.media_id_string;
-      const meta_params = { media_id: mediaIdStr };
-  
-      pollStatus(bot, mediaIdStr, 0, function() {
-        // Create media metadata.
-        bot.post('media/metadata/create', meta_params, function (err, data, response) {
-          console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-          if (!err) {
-            // Make the tweet params.
-            var params = { media_ids: [mediaIdStr] }
-      
-            bot.post('statuses/update', params, function (err, data, response) {
-              logger.info(`Main tweet posted: https://twitter.com/${config.botUsername}/status/${data.id_str}`)
-
-              // Log tweet data (FOR DEBUG)
-              // logger.info("statuses/update data:")
-              // console.log(data)
-
-              var mainTweet = data.id_str;
-
-              bot.post('statuses/update', {status: `[source: https://bumpworthy.com/bumps/${bumpNum}]`, in_reply_to_status_id: mainTweet}, function (err, data, response) {
-                logger.info(`Reply tweet posted: https://twitter.com/${config.botUsername}/status/${data.id_str}`)
-              
-                // Log tweet data (FOR DEBUG)
-                // logger.info("statuses/update data:")
-                // console.log(data)
-              });
-            });
-          } else {
-            // If something happened.
-            logger.error("media/metadata/create error:")
-            console.log(err)
-          }
-        });
-      });
-    });
-  });
-});
 
 console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+logger.info(`Starting process...`)
+// START
+// Time for how long until new clip is uploaded
+var time = config.time * 60000;
+// Start function
+function startProcess() {
 
+    // Make video file.
+    var bumpNum = getRandomArbitrary(1, 8749);;
+    var url = `http://static.bumpworthy.com/bumps/${bumpNum}.d.mp4` // Get bump video file.
+    var filepath = `media/${bumpNum}.mp4` // Set export locaiton.
 
-// Get media status from Twitter for uploading media.
-function pollStatus(bot, mediaIdStr, progressPercent, callback) {
-  bot.get("media/upload", { command: "STATUS", media_id: mediaIdStr }, function(err, data, response) {
-    // Log media data (FOR DEBUG)
-    // console.log(data)
-
-    if (progressPercent == 2) {
-      logger.error(`Media failed, the video can not be uploaded. Restart to try again.`)
-      return
+    // Check to see if the video already exists.
+    var checkFile = fs.existsSync(filepath)
+    if (checkFile = false) {
+        logger.error(`Media failed, a video file with the name is in the "media" folder. Restart to try again.`)
+        return;
     }
 
-    if (err) {
-      // If something happened.
-      logger.error("pollStatus error:")
-      console.log(err)
-      return
-    } else {
-      if (data && data.processing_info && data.processing_info.state === "succeeded") { // When the media is uploaded.
-        logger.info("Media uploaded.")
-        callback(); // Go back to main code.
-      } else {
-        logger.info(`Media is uploading. Progress: ${data.processing_info.progress_percent}, retrying...`)
+    // Download video.
+    const file = fs.createWriteStream(filepath);
+    const thing = http.get(url, (response) => {
+        response.pipe(file);
 
-        if (data.processing_info.progress_percent == "100") {
-          progressPercent++
-        }
+        file.on('finish', async () => { // When file is done downloaded.
+            file.close();
 
-        setTimeout(() => {pollStatus(bot, mediaIdStr, progressPercent, callback, err)}, 1000); // Retry after the secs provided by Twitter.
-      }
-    }
-  })
-}
+            if (file.bytesWritten <= 500) {
+                logger.error(`The bump received (ID: ${bumpNum}) might be not be an actual file.`)
+                logger.error(`At the moment, the only way to solve this is to restart, sorry... :(`)
+                return;
+            }
+
+            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+            logger.info('Download Completed');
+
+            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+            logger.info('Upload Started...');
+
+            var filePath = `./${filepath}`;
+
+            // Upload media first...
+            const mediaIds = await Promise.all([
+                client.v1.uploadMedia(filePath),
+            ]);
+            logger.info('Upload Completed');
+
+            // Then we can send in the tweet.
+            var mainTweet = await client.v1.tweet('', { media_ids: mediaIds })
+            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+            // get bot username for logs
+            var botUsername = await getUsername();
+            logger.info(`Sent main tweet; https://twitter.com/${botUsername}/status/${mainTweet.id_str}`);
+
+            // Oh! Don't forget the reply!
+            var replyTweet = await client.v1.reply(`[source: https://bumpworthy.com/bumps/${bumpNum}]`, mainTweet.id_str);
+            logger.info(`Sent reply tweet; https://twitter.com/${botUsername}/status/${replyTweet.id_str}`);
+        });
+    });
+};
+
+// Start the bot when first loaded.
+startProcess();
+
+// After a while, start again.
+setInterval(() => {
+    console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+    logger.info(`Time hit! Redoing process...`)
+    startProcess();
+}, time);
