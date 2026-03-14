9a6e278affd4e23ee9cb7561adaa7b0d8a5142b0
kyle <42522603+ktg5@users.noreply.github.com>
2026-03-14 11:19:54 -0700

    2.3.0: HTTP to HTTPS, better scheduling, & callbacks
    updating one of my old projects xd

--- bot.js
+++ bot.js
@@ -1,90 +1,133 @@
-const {client, logger} = require("./botAuth");
-const http = require('http');
+const { client, logger } = require("./botAuth");
+const https = require('https');
 const fs = require('fs');
 const schedule = require('node-schedule');
+const config = require('./config.json');
 
 // getRandomArbitrary
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Get bot Username (can only be called when Twitter wakes up)
-async function getUsername() {
-    return (await client.v2.me()).data.username;
+// Check if the user is stupid
+if (config.hours.includes('.')) {
+    logger.error(`Don't put dots in the f**kin' hours value in your config! Use the "minutes" value instead!!!`);
+    process.exit();
 }
 
 console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-logger.info(`Starting process every hour...`)
+logger.info(config.everyHourandaHour ? "Starting process when it hits every hour and a half..." : `Starting process when it hits every ${config.hours} hour(s)...`);
 
+
+// Make sure the media folder exists
+const mediaFolder = fs.existsSync('media/');
+if (mediaFolder === false) {
+    logger.info('Creating "media" folder...');
+    fs.mkdirSync('media');
+    logger.info('Created "media" folder!');
+}
+
+
+var currentMedia;
+var restartCount = 0;
 
 // Start function
-function startProcess() {
+function startProcess(defiendClip) {
 
     // Make video file.
     var bumpNum = getRandomArbitrary(1, 8749);;
-    var url = `http://static.bumpworthy.com/bumps/${bumpNum}.d.mp4` // Get bump video file.
+    var url = `https://static.bumpworthy.com/bumps/${bumpNum}.d.mp4` // Get bump video file.
     var filepath = `media/${bumpNum}.mp4` // Set export locaiton.
 
-    // Check to see if the video already exists.
-    var checkFile = fs.existsSync(filepath)
-    if (checkFile = false) {
-        logger.error(`Media failed, a video file with the name is in the "media" folder. Restart to try again.`)
-        return;
-    }
+    // If the proccess was possibly restarted, we'll skip another download
+    if (!defiendClip) {
+        // Download video.
+        const file = fs.createWriteStream(filepath);
+        const thing = https.get(url, (res) => {
+            res.pipe(file);
 
-    // Download video.
-    const file = fs.createWriteStream(filepath);
-    const thing = http.get(url, (response) => {
-        response.pipe(file);
+            file.on('finish', async () => { // When file is done downloaded.
+                file.close();
 
-        file.on('finish', async () => { // When file is done downloaded.
-            file.close();
+                if (file.bytesWritten <= 500) {
+                    logger.error(`The bump received (ID: ${bumpNum}) might be not be an actual file.`)
+                    if (restartCount >= 5) {
+                        logger.error(`Too many restarts! Not going to continue.`);
+                        return restartCount = 0;
+                    } else {
+                        logger.error(`Restarting...`);
+                        startProcess();
+                        restartCount++;
+                    }
+                } else currentMedia = filepath;
 
-            if (file.bytesWritten <= 500) {
-                logger.error(`The bump received (ID: ${bumpNum}) might be not be an actual file.`)
-                logger.error(`At the moment, the only way to solve this is to restart, sorry... :(`)
-                return;
-            }
-
-            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-            logger.info('Download Completed');
-
-            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-            logger.info('Upload Started...');
-
-            var filePath = `./${filepath}`;
-
-            // Now we're gonna try to upload the media & tweets.
-            try {
-                
-                // Upload media first...
-                const mediaIds = await Promise.all([
-                    client.v1.uploadMedia(filePath),
-                ]);
-                logger.info('Upload Completed');
-
-                // Then we can send in the tweet.
-                var mainTweet = await client.v2.tweet('', { media: { media_ids: mediaIds } })
                 console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
-                // get bot username for logs
-                var botUsername = await getUsername();
-                logger.info(`Sent main tweet; https://twitter.com/${botUsername}/status/${mainTweet.data.id}`);
+                logger.info(`Download Completed - Bump #${bumpNum}`);
 
-                // Oh! Don't forget the reply!
-                var replyTweet = await client.v2.reply(`[source: https://bumpworthy.com/bumps/${bumpNum}]`, mainTweet.data.id);
-                logger.info(`Sent reply tweet; https://twitter.com/${botUsername}/status/${replyTweet.data.id}`);
-            
-            } catch (error) {
-                logger.error(`Something happened while uploading / trying to tweet!!`);
-                logger.error(error);
-            }
+                getToTweeting(filepath);
+            });
         });
-    });
+    } else getToTweeting(defiendClip);
+
+
+    async function getToTweeting(filepath) {
+        console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+        logger.info('Upload Started...');
+
+        var dotfilepath = `./${filepath}`;
+
+        // Now we're gonna try to upload the media & tweets.
+        try {
+            
+            // Upload media first...
+            let mediaId = await client.v1.uploadMedia(dotfilepath);
+            logger.info('Upload Completed -', mediaId);
+
+            // Add alt text.
+            let altTxt = '';
+            if (!config.addReplyInsteadofAlt || config.addReplyInsteadofAlt != true) altTxt = `[source: https://bumpworthy.com/bumps/${bumpNum}]`;
+
+            // Then we can send in the tweet.
+            var mainTweet = await client.v2.tweet(altTxt, { media: { media_ids: [mediaId] } });
+            console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+            logger.info(`Sent main tweet; https://twitter.com/${config.username}/status/${mainTweet.data.id}`);
+
+            // Reply if user wants to use up rate limits.
+            if (config.addReplyInsteadofAlt == true) {
+                var replyTweet = await client.v2.reply(`[source: https://bumpworthy.com/bumps/${bumpNum}]`, mainTweet.data.id);
+                logger.info(`Sent reply tweet; https://twitter.com/${config.username}/status/${replyTweet.data.id}`);
+            }
+        
+        } catch (error) {
+            logger.error(`Tweet failed! "${error.message}"`);
+            if (restartCount >= 5) {
+                logger.error(`Too many restarts! Not going to continue.`);
+                return restartCount = 0;
+            } else {
+                logger.error(`Restarting...`);
+                startProcess(filepath);
+                restartCount++;
+            }
+            return;
+        }
+    }
 };
 
-// After it hits an hour, start again.
-var j = schedule.scheduleJob('0 */1 * * *', function(){  // this for one hour
+// Start the process for dev purposes.
+// startProcess();
+
+// After it hits the every hour the user defined, start again.
+let nodeSchedule = `* */${config.hours} * * *`;
+if (config.everyHourandaHour) nodeSchedule = `0 */3 * * *`;
+var j = schedule.scheduleJob(nodeSchedule, () => {  // this for one hour
     console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
     logger.info(`Time hit! Redoing process...`)
     startProcess();
 });
+if (config.everyHourandaHour) {
+    var l = schedule.scheduleJob("30 1-23/3 * * *", () => {  // this for one hour
+        console.log(`ｰｰｰｰｰｰｰｰｰｰ✄ｰｰｰｰｰｰｰｰｰｰ`);
+        logger.info(`Time hit! Redoing process...`)
+        startProcess();
+    });
+}
